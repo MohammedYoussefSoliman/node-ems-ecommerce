6b212a89cd03fef9b732c5b2f9b2b903419d79cd
MohammedYoussefSoliman <m.youssef.soliman@gmail.com>
2023-03-23 02:56:03 +0200

    added helper middlewares

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,34 @@
 import 'module-alias/register'
 import express from 'express'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import rateLimit from 'express-rate-limit'
 import { configEnvs } from '@utils/index'
 
 configEnvs()
 const app = express()
+app.use(express.json())
+app.use(helmet())
+app.use(
+  rateLimit({
+    windowMs: 60 * 1000,
+    max: 20,
+    standardHeaders: true,
+    message: 'Too many requests',
+  })
+)
+app.use(morgan('common'))
 
 app.get('/', (req, res) => {
   res.json({
-    message: 'hello world ',
+    message: 'hello world',
+  })
+})
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.json({
+    message: 'hello from post',
+    data: req.body,
   })
 })
 
